d74a21875939bc90d4c9afb14a2fef53a3ec441c
rishaiittp <59837540+rishaiittp@users.noreply.github.com>
2020-01-31 17:28:13 +0530

    Update news.java

--- news.java
+++ news.java
@@ -1,1 +1,10 @@
 <p>October 2019: Akhila Sri Manasa is promoted to Ph.D.</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
+<p>October 2019: Congratulations!</p>
