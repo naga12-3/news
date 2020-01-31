d72a59d4dc3598aa67f7b2857cb0ca8b7d42c5d9
rishaiittp <59837540+rishaiittp@users.noreply.github.com>
2020-01-31 19:39:25 +0530

    Update news.java

--- news.java
+++ news.java
@@ -1,4 +1,4 @@
-<p style="color: white;border-radius: 0.2rem 0rem 0rem 0.2rem;background: #505050;display: inline;font-size: medium;padding-left: 0.5rem;padding-top: 0.2rem;padding-bottom: 0.2rem;padding-right: 0.5rem;border: 0px;margin-right: 0px;">
+<p style="color: white;border-radius: 0.2rem 0rem 0rem 0.2rem;background: #505050;font-size: medium;padding-left: 0.5rem;padding-top: 0.2rem;padding-bottom: 0.2rem;padding-right: 0.5rem;border: 0px;margin-right: 0px;">
 JAN 31, 2020 <span style="margin-left:3rem;"> October 2019: Akhila Sri Manasa is promoted to Ph.D.</span>
 </p>
 <p>October 2019: Akhila Sri Manasa is promoted to Ph.D.</p>
