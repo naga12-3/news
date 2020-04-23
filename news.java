c71b79861ca56e0d656b9eeddb8fb44edeb406e7
rishaiittp <59837540+rishaiittp@users.noreply.github.com>
2020-04-23 18:16:02 +0530

    Update and rename news.java to news.html

--- news.java
+++
@@ -1,1 +0,0 @@
-<p><div style="margin-right:50rem;width:30rem"><div style="margin-left:10rem"><time datetime="2014-09-20" class="icon"><em>Saturday</em><strong>September  <br>2020</strong><span>20</span></time></div><div style="color: black;font-size: 1.5rem;vertical-align: middle;padding-top: 2.5rem;">Paper on Live Energies got published in ACM</div></div></p>
